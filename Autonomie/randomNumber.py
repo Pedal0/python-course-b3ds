4013aaaf5822e551c81655ef9f7582731e519b4b
Pedal0 <perax33@gmail.com>
2024-09-30 11:56:50 +0200

    variable name snake case

--- Autonomie/randomNumber.py
+++ Autonomie/randomNumber.py
@@ -1,21 +1,25 @@
 import random
-difficulty = [10,100,1000]
-chooseDifficulty = int(input("Difficulté 0: 0 a 10 \n Difficulté 1: 0 a 100 \n Difficulté 2: 0 a 1000:\n")) 
-userNumber = int(input("Entrez votre nombre:"))
-selectedDifficulty = difficulty[chooseDifficulty]
-randomNumber = random.randint(0,selectedDifficulty)
+
+
 coup = 1
-print(randomNumber)
-while userNumber != randomNumber :
-    if userNumber < randomNumber :
+difficulty = [10,100,1000]
+choose_difficulty = int(input("Difficulté 0: 0 a 10 \n Difficulté 1: 0 a 100 \n Difficulté 2: 0 a 1000:\n")) 
+user_number = int(input("Entrez votre nombre:"))
+selected_difficulty = difficulty[choose_difficulty]
+random_number = random.randint(0,selected_difficulty)
+
+print(random_number)
+
+while user_number != random_number :
+    if user_number < random_number :
         print("Trop petit")
         coup += 1
-        userNumber = int(input("Entrez votre nombre:"))
-    elif userNumber > randomNumber : 
+        user_number = int(input("Entrez votre nombre:"))
+    elif user_number > random_number : 
         print("Trop grand")
         coup += 1
-        userNumber = int(input("Entrez votre nombre:"))
+        user_number = int(input("Entrez votre nombre:"))
 
-print("Le nombre est bien: " + str(randomNumber))
+print("Le nombre est bien: " + str(random_number))
 print("Le nombre de coup jouer est de: " + str(coup))
 
